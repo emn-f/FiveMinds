13bcc59be7a84a3b3cc37ce9c3c339efed028308
emanuel <95362058+emn-f@users.noreply.github.com>
2025-11-28 18:31:08 -0300

    refactor: ajustes básicos

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
     const loginButton = document.getElementById('login-button');
     const loginMessage = document.getElementById('login-message');
 
-    loginButton.addEventListener('click', function(event) {
-        event.preventDefault(); // Impede o envio do formulário padrão
+    loginButton.addEventListener('click', function (event) {
+        event.preventDefault();
 
         const username = usernameInput.value;
         const password = passwordInput.value;
 
-        // Lógica de autenticação
         if (username === 'five' && password === 'minds') {
-            loginMessage.textContent = ''; // Limpa qualquer mensagem de erro
-            window.location.href = 'dash.html'; // Redireciona para a página principal
+            loginMessage.textContent = '';
+            window.location.href = 'dash.html';
         } else {
             loginMessage.textContent = 'Usuário ou senha incorretos.';
         }
